a5a2db11c518244a58fc3888b19d285eb8dfcd1d
LuisACampos7 <00122425@uca.edu.sv>
2025-06-03 09:50:39 -0600

    add file :memo:

---
+++ ejercicios_for/Ejercicios.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int tarifa;
+    cout <<"ingrese su tarifa"<<endl;
+    cin>> tarifa;
+
+    if(tarifa < 100){
+        cout <<"";
+    } 
+    else if()
+
+
+    return 0;
+}
